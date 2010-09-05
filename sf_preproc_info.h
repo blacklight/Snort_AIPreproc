7174b93511aaddb1385701c8d096f5301e644fa7
BlackLight <blacklight@autistici.org>
2010-09-05 15:27:35 +0200

    Using autotools now

--- sf_preproc_info.h
+++ sf_preproc_info.h
@@ -1,12 +1,13 @@
-#ifndef SF_PREPROC_INFO_H_
-#define SF_PREPROC_INFO_H_
+#ifndef 	SF_PREPROC_INFO_H_
+#define 	SF_PREPROC_INFO_H_
 
-#define MAJOR_VERSION   1
-#define MINOR_VERSION   0
-#define BUILD_VERSION   1
-#define PREPROC_NAME    "SF_AI"
+// #define 	VERSION 				"0.1.0"
+#define 	MAJOR_VERSION 			0
+#define 	MINOR_VERSION 			1
+#define 	BUILD_VERSION 			0
+#define 	PREPROC_NAME 			"SF_AI"
 
-#define DYNAMIC_PREPROC_SETUP   AI_setup
+#define 	DYNAMIC_PREPROC_SETUP 	AI_setup
 extern void AI_setup();
 
 #endif /* SF_PREPROC_INFO_H_ */
